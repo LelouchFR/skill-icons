047ef9febdb299190b6964881fdc33cd56d60a73
Hawli <126889064+hawl1@users.noreply.github.com>
2024-05-05 16:42:04 +0000

    reforkingwrite

--- api/index.go
+++ api/index.go
@@ -1,17 +1,23 @@
-package api
+package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-var skillAliases = map[string]string{
+var icons map[string]string = make(map[string]string)
+var iconNameList []string
+var themedIcons []string
+
+
+
+var shortNames = map[string]string{
 	"js":                "javascript",
 	"ts":                "typescript",
 	"py":                "python",
@@ -83,115 +89,144 @@ var (
 	app *gin.Engine
 )
 
-func resolveSkillAlias(alias string) string {
-	if skill, ok := skillAliases[alias]; ok {
-		return skill
-	}
-	return alias
-}
-
-func GetSkillIcons(ctx *gin.Context) {
-	skills := ctx.Query("i")
-	theme := ctx.Query("theme")
-	perLineQuery := ctx.Query("perline")
-	hasTitleQuery := ctx.Query("titles")
-
-	perLine, err := strconv.Atoi(perLineQuery)
-	if err != nil {
-		perLine = IconsPerLine
-	}
-
-	hasTitle, err := strconv.ParseBool(hasTitleQuery)
-	if err != nil {
-		hasTitle = false
-	}
-
-	if theme == "" {
-		theme = "dark"
-	} else {
-		theme = "light"
-	}
-
-	skillNames := strings.Split(skills, ",")
-
-	for i, skill := range skillNames {
-		skillNames[i] = resolveSkillAlias(skill)
-	}
-
-	svg := GenerateSVG(skillNames, theme, hasTitle, perLine)
-
-	ctx.Data(http.StatusOK, "image/svg+xml", []byte(svg))
-}
-
 const (
-	IconsPerLine = 15
-	OneIcon      = 48
-	Scale        = 0.1875
+	ICONS_PER_LINE = 15
+	ONE_ICON       = 48
+	SCALE          = float64(ONE_ICON) / float64(300-44)
 )
 
-func GenerateSVG(iconNames []string, theme string, hasTitle bool, perLine int) string {
-	var svg strings.Builder
+func generateSvg(iconNames []string, perLine int) string {
+	iconSvgList := make([]string, len(iconNames))
 
-	length := math.Min(float64(perLine*300), float64(len(iconNames)*300)) - 44
-	height := math.Ceil(float64(len(iconNames))/float64(perLine))*300 - 44
-	scaledWidth := length * Scale
-	scaledHeight := height * Scale
-
-	svg.WriteString(fmt.Sprintf(`<svg width="%f" height="%f" viewBox="0 0 %f %f" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">`, scaledWidth, scaledHeight, length, height))
-
-	for idx, iconName := range iconNames {
-		dirPath := "."
-
-		// Read the directory
-		files, err := os.ReadDir(dirPath)
-		if err != nil {
-			fmt.Println("Error reading directory:", err)
-		}
-
-		// Iterate over the files
-		fmt.Println("Files and folders in", dirPath+":")
-		for _, file := range files {
-			// Check if it's a file or a directory
-			if file.IsDir() {
-				fmt.Println("Directory:", file.Name())
-			} else {
-				fmt.Println("File:", file.Name())
-			}
-		}
-
-		var title string
-		if hasTitle {
-			title = fmt.Sprintf("<title>%s</title>", iconName)
-		}
-
-		transformX := (idx % perLine) * 300
-		transformY := (idx / perLine) * 300
-
-		svg.WriteString(fmt.Sprintf(`
-            <g transform="translate(%d, %d)">
-                %s
-                %s
-            </g>
-        `, transformX, transformY, title, string("zaza")))
+	for i, name := range iconNames {
+		iconSvgList[i] = icons[name]
 	}
 
-	svg.WriteString(`</svg>`)
+	length := int(math.Min(float64(perLine*300), float64(len(iconNames)*300))) - 44
+	height := int(math.Ceil(float64(len(iconSvgList))/float64(perLine)))*300 - 44
+	scaledHeight := int(float64(height) * SCALE)
+	scaledWidth := int(float64(length) * SCALE)
 
-	return svg.String()
+	svg := fmt.Sprintf(`
+	<svg width="%d" height="%d" viewBox="0 0 %d %d" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+	`, scaledWidth, scaledHeight, length, height)
+
+	for index, i := range iconSvgList {
+		x := (index % perLine) * 300
+		y := (index / perLine) * 300
+		svg += fmt.Sprintf(`
+		<g transform="translate(%d, %d)">
+			%s
+		</g>
+		`, x, y, i)
+	}
+
+	svg += "</svg>"
+	return svg
 }
 
-func myRoute(r *gin.RouterGroup) {
+func parseShortNames(names []string, theme string) []string {
+	result := make([]string, len(names))
+
+	for i, name := range names {
+		if contains(iconNameList, name) {
+			if contains(themedIcons, name) {
+				result[i] = name + "-" + theme
+			} else {
+				result[i] = name
+			}
+		} else if val, ok := shortNames[name]; ok {
+			if contains(themedIcons, val) {
+				result[i] = val + "-" + theme
+			} else {
+				result[i] = val
+			}
+		}
+	}
+	return result
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
+func iconRoute(r *gin.RouterGroup) {
 	r.GET("/icons", func(ctx *gin.Context) {
-		GetSkillIcons(ctx)
+
+		ctx.Request.ParseForm()
+		iconParam := ctx.Request.Form.Get("i")
+
+		theme := ctx.Request.Form.Get("t")
+		if theme == "" {
+			theme = "dark"
+		}
+
+		perLineStr := ctx.Request.Form.Get("perline")
+		if perLineStr == "" {
+			perLineStr = "15"
+		}
+
+		if iconParam == "" {
+			ctx.String(http.StatusBadRequest, "You didn't specify any icons!")
+			return
+		}
+
+		if theme != "dark" && theme != "light" && theme != "" {
+			ctx.String(http.StatusBadRequest, "Theme must be either 'light' or 'dark'")
+			return
+		}
+
+		perLine, err := strconv.Atoi(perLineStr)
+		if err != nil || perLine < -1 || perLine > 50 {
+			ctx.String(http.StatusBadRequest, "Icons per line must be a number between 1 and 50")
+			return
+		}
+
+		var iconShortNames []string
+		if iconParam == "all" {
+			iconShortNames = iconNameList
+		} else {
+			iconShortNames = strings.Split(iconParam, ",")
+		}
+
+		iconNames := parseShortNames(iconShortNames, theme)
+		if iconNames == nil {
+			ctx.String(http.StatusBadRequest, "You didn't format the icons param correctly!")
+			return
+		}
+
+		svg := generateSvg(iconNames, perLine)
+
+		ctx.Header("Content-Type", "image/svg+xml")
+		ctx.String(http.StatusOK, svg)
 	})
 }
 
 func init() {
 	app = gin.New()
 	r := app.Group("/api")
-	myRoute(r)
+
+	iconRoute(r)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(strings.NewReader(iconsJSON))
+	if err := decoder.Decode(&icons); err != nil {
+		panic(err)
+	}
+
+	// Populate iconNameList and themedIcons
+	for key := range icons {
+		iconNameList = append(iconNameList, strings.Split(key, "-")[0])
+		if strings.Contains(key, "-light") || strings.Contains(key, "-dark") {
+			themedIcons = append(themedIcons, strings.Split(key, "-")[0])
+		}
+	}
+
 	app.ServeHTTP(w, r)
 }
---
+++ build.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Read directory contents
+	iconsDir, err := os.ReadDir("./assets")
+	if err != nil {
+		panic(err)
+	}
+
+	// Initialize map for icons
+	icons := make(map[string]string)
+
+	// Iterate through directory contents
+	for _, fileInfo := range iconsDir {
+		if !fileInfo.IsDir() {
+			// Read file contents
+			data, err := os.ReadFile("./assets/" + fileInfo.Name())
+			if err != nil {
+				panic(err)
+			}
+
+			// Add file content to map
+			name := strings.TrimSuffix(strings.ToLower(fileInfo.Name()), ".svg")
+			icons[name] = string(data)
+		}
+	}
+
+	// Convert icons to JSON
+	iconsJSON, err := json.Marshal(icons)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read content of api/index.go
+	indexFile, err := os.ReadFile("./api/index.go")
+	if err != nil {
+		panic(err)
+	}
+
+	// Convert content to string
+	indexContent := string(indexFile)
+
+	// Split the content by newlines
+	lines := strings.Split(indexContent, "\n")
+
+	// Insert the iconsJSON into line 18
+	lines[17] = fmt.Sprintf("\n\tvar iconsJSON = `%s`\n", iconsJSON)
+
+	// Join the lines back into a single string
+	modifiedContent := strings.Join(lines, "\n")
+
+	// Write the modified content back to the file
+	err = os.WriteFile("./api/index.go", []byte(modifiedContent), 0644)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Modified content saved to api/index.go")
+}
