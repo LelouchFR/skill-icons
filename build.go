ff87c7f814c5c95c40abfad6bfa204d006c864a3
Baptiste Zahnow <75901978+LelouchFR@users.noreply.github.com>
2024-05-11 19:00:09 +0200

    Revert "fix: implement more healthly way to access iconsjson string (#17)"
    This reverts commit 2d3106632fb67c9ccc1bbc7d9f22d433614161e0.

--- build.go
+++ build.go
@@ -8,50 +8,59 @@ import (
 )
 
 func main() {
+	// Read directory contents
 	iconsDir, err := os.ReadDir("./assets")
 	if err != nil {
 		panic(err)
 	}
 
+	// Initialize map for icons
 	icons := make(map[string]string)
 
+	// Iterate through directory contents
 	for _, fileInfo := range iconsDir {
 		if !fileInfo.IsDir() {
+			// Read file contents
 			data, err := os.ReadFile("./assets/" + fileInfo.Name())
 			if err != nil {
 				panic(err)
 			}
 
+			// Add file content to map
 			name := strings.TrimSuffix(strings.ToLower(fileInfo.Name()), ".svg")
 			icons[name] = string(data)
 		}
 	}
 
+	// Convert icons to JSON
 	iconsJSON, err := json.Marshal(icons)
 	if err != nil {
 		panic(err)
 	}
 
-	indexFile, err := os.ReadFile("./api/icons.go")
+	// Read content of api/index.go
+	indexFile, err := os.ReadFile("./api/index.go")
 	if err != nil {
 		panic(err)
 	}
 
+	// Convert content to string
 	indexContent := string(indexFile)
 
+	// Split the content by newlines
 	lines := strings.Split(indexContent, "\n")
 
 	// Insert the iconsJSON into line 18
-	lines[6] = fmt.Sprintf("\n\tvar iconsJSON string = `%s`\n", iconsJSON)
+	lines[17] = fmt.Sprintf("\n\tvar iconsJSON = `%s`\n", iconsJSON)
 
 	// Join the lines back into a single string
 	modifiedContent := strings.Join(lines, "\n")
 
 	// Write the modified content back to the file
-	err = os.WriteFile("./api/icons.go", []byte(modifiedContent), 0644)
+	err = os.WriteFile("./api/index.go", []byte(modifiedContent), 0644)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("Modified content saved to api/icons.go")
+	fmt.Println("Modified content saved to api/index.go")
 }
