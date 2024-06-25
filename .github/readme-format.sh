d6086248c3ef877db16ea5f4254649b71cb88376
Hawli <126889064+hawl1@users.noreply.github.com>
2024-06-25 04:51:02 +0000

    format bash

--- .github/readme-format.sh
+++ .github/readme-format.sh
@@ -1,4 +1,4 @@
-head -96 README.md > README.tmp && mv README.tmp README.md
+head -96 README.md >README.tmp && mv README.tmp README.md
 
 icons=$(ls assets --ignore="*-light.svg" --ignore="*-dark.svg")
 icon_list=($(ls assets --ignore="*-light.svg" --ignore="*-dark.svg"))
@@ -6,8 +6,7 @@ icon_list=($(ls assets --ignore="*-light.svg" --ignore="*-dark.svg"))
 function get_maximum_length() {
     items=$1
     maximum=0
-    for item in $items
-    do
+    for item in $items; do
         if [[ ${#item} -gt $maximum ]]; then
             maximum=${#item}
         fi
@@ -17,38 +16,34 @@ function get_maximum_length() {
 
 icons_counter=0
 declare -a img_tags
-for icon in $icons
-do
+for icon in $icons; do
     icons_counter=$((icons_counter + 1))
     img_tags+=("<img src=\"./assets/$icon\" width=\"48\">")
 done
 
 table_headers=("" "")
 columns=$(($icons_counter / 100 + 1))
-for column in $(seq 1 $columns)
-do
+for column in $(seq 1 $columns); do
     table_headers[0]+="| Icon ID | Icon "
     table_headers[1]+="| :-----------------: | :--------------: "
 done
 table_headers[0]+="|"
 table_headers[1]+="|"
 
-echo ${table_headers[0]} >> README.md
-echo ${table_headers[1]} >> README.md
+echo ${table_headers[0]} >>README.md
+echo ${table_headers[1]} >>README.md
 
 max_icon_id_length=$(get_maximum_length "$icons")
 max_img_tag_length=$(get_maximum_length "${img_tags[*]}")
 
-# Gonna make this because stupid bash script cannot calculate $max_img_tag_length correctly, such an idiot.
-
+# Gonna make this because stupid bash script cannot calculate $max_img_tag_length correctly, fucking idiot.
 most_negative_number=0
 count_id=0
 for row in $(seq 1 100); do
     img_tag="<img src=\"./assets/${icon_list[$(($count_id))]}\">"
-    padding_img_tag=$(( (max_img_tag_length - ${#img_tag} + 1) / 2 ))
+    padding_img_tag=$(((max_img_tag_length - ${#img_tag} + 1) / 2))
     count_id=$((count_id + 1))
 
-    # Check if padding_img_tag is less than most_negative_number
     if [[ $padding_img_tag -gt $most_negative_number ]]; then
         most_negative_number=$padding_img_tag
     fi
@@ -56,10 +51,8 @@ done
 
 count_id=0
 declare -a icon_table
-for column in $(seq 1 $columns)
-do
-    for row in $(seq 1 100)
-    do
+for column in $(seq 1 $columns); do
+    for row in $(seq 1 100); do
         count_id=$(($count_id + 1))
         if [[ $count_id -ge $icons_counter ]]; then
             continue
@@ -67,8 +60,8 @@ do
         icon_id=$(echo ${icon_list[$(($count_id))]} | sed 's/\.[^.]*$//; s/-auto//g')
         img_tag="<img src=\"./assets/${icon_list[$(($count_id))]}\" width=\"48\">"
 
-        padding_icon_id=$(( (max_icon_id_length - ${#icon_id} + 1) / 2 ))
-        padding_img_tag=$(( (max_img_tag_length - ${#img_tag} + 1) / 2 ))
+        padding_icon_id=$(((max_icon_id_length - ${#icon_id} + 1) / 2))
+        padding_img_tag=$(((max_img_tag_length - ${#img_tag} + 1) / 2))
         padding_img_tag=$((padding_img_tag + most_negative_number))
 
         padded_icon_id=$(printf "%-${padding_icon_id}s%s%${padding_icon_id}s" "" "\`$icon_id\`" "")
@@ -82,7 +75,7 @@ do
             padded_img_tag+=" "
         fi
 
-        if  [[ ${#padded_img_tag} -eq 54 ]]; then
+        if [[ ${#padded_img_tag} -eq 54 ]]; then
             padded_img_tag+="  "
         fi
 
@@ -90,12 +83,11 @@ do
     done
 done
 
-for column in $(seq 1 100)
-do
-    echo "${icon_table[$column]}|" >> README.md
+for column in $(seq 1 100); do
+    echo "${icon_table[$column]}|" >>README.md
 done
 
-echo "" >> README.md
-echo "## 💖 Support the Project" >> README.md
-echo "Thank you so much already for using my projects!" >> README.md
-echo "To support the project directly, feel free to open issues for icon suggestions, or contribute with a pull request!" >> README.md
+echo "" >>README.md
+echo "## 💖 Support the Project" >>README.md
+echo "Thank you so much already for using my projects!" >>README.md
+echo "To support the project directly, feel free to open issues for icon suggestions, or contribute with a pull request!" >>README.md
